9ffc5170522717bf45cb110ab5ccde5f13edfce8
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-05-17 16:44:58 +0530

    user data uploaded on firebase

--- app/src/main/java/com/taxonteam/taxon/dashboard.kt
+++ app/src/main/java/com/taxonteam/taxon/dashboard.kt
@@ -6,13 +6,24 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.taxonteam.taxon.ui.dataRecorder
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.content_dashboard.*
 import java.io.File
 
 class dashboard : AppCompatActivity() {
-
+    val mRef=FirebaseDatabase.getInstance().getReference()
+    val mAuth=FirebaseAuth.getInstance()
+    val userid=mAuth.currentUser?.uid
+    var name:String=""
+    var uid:String=""
+    var email:String=""
+    var phone:String=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -23,22 +34,44 @@ class dashboard : AppCompatActivity() {
 
         setNavigationDrawer();
 
-        val dataDir=ContextCompat.getDataDir(this)
-        val dataFile=File(dataDir,"userdata.txt")
-        if(dataFile.exists()){
-            //get all the data u needed
-            var data = dataFile.readText()
-            var datalist = data.split("/n")
-            for (i in 0..datalist.size-1)
-                Log.i("userdata",datalist[i])
-        }
-        else{
+        //mRef.child("users").child(userid.toString()).child("email").setValue("test@gmail.com")
+        val searchListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    //Key exists
+                    Log.i("keysearch","reading");
+                    fetchUserData()
 
-            supportFragmentManager.beginTransaction().add(R.id.datafrag,
-                dataRecorder()
-            ).commitNow()
-            //dataFile.writeText("Abhishek/nChandigarh University/nstudent/n")
-            Log.i("userdata","writing")
+                } else {
+                    //Key does not exist
+                    Log.i("keysearch","writing");
+                    supportFragmentManager.beginTransaction().add(R.id.datafrag, dataRecorder()
+                    ).commitNow()
+                    fetchUserData()
+
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) { }
+        }
+        mRef.child("users").orderByKey().equalTo(userid).addValueEventListener(searchListener)
+
+
+    }
+
+    private fun fetchUserData() {
+        val dataListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                name= dataSnapshot.child("name").getValue(String::class.java).toString()
+                uid= dataSnapshot.child("uid").getValue(String::class.java).toString()
+                email= dataSnapshot.child("email").getValue(String::class.java).toString()
+                phone= dataSnapshot.child("phone").getValue(String::class.java).toString()
+                //Log.i("datafetched",data)
+                Log.i("keysearch",name+uid+email+phone);
+            }
+            override fun onCancelled(databaseError: DatabaseError) { }
+        }
+        if (userid != null) {
+            mRef.child("users").child(userid).addValueEventListener(dataListener)
         }
     }
 
@@ -58,5 +91,7 @@ class dashboard : AppCompatActivity() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
     }
+
+
 
 }
--- app/src/main/java/com/taxonteam/taxon/ui/dataRecorder.kt
+++ app/src/main/java/com/taxonteam/taxon/ui/dataRecorder.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.taxonteam.taxon.R
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.content_dashboard.*
@@ -14,7 +16,9 @@ import kotlinx.android.synthetic.main.fragment_data_recorder.*
 import java.io.File
 
 class dataRecorder : Fragment() {
-
+    val mRef= FirebaseDatabase.getInstance().getReference()
+    val mAuth= FirebaseAuth.getInstance()
+    val userid=mAuth.currentUser?.uid
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,13 +30,16 @@ class dataRecorder : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         savebtn.setOnClickListener {
-            val dataDir= ContextCompat.getDataDir(requireContext())
-            val dataFile= File(dataDir,"userdata.txt")
+
             val name =nameet.text.toString()
-            val UID =uidet.text.toString()
+            val uid =uidet.text.toString()
+            val email =emailuseret.text.toString()
             val phone =phoneet.text.toString()
-            dataFile.writeText(name+"\n"+UID+"\n"+phone)
-            (datafrag.getParent() as ViewGroup).removeView(datafrag)
+            mRef.child("users").child(userid.toString()).child("name").setValue(name)
+            mRef.child("users").child(userid.toString()).child("uid").setValue(uid)
+            mRef.child("users").child(userid.toString()).child("email").setValue(email)
+            mRef.child("users").child(userid.toString()).child("phone").setValue(phone)
+
         }
     }
 
